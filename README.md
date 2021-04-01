e95b64b6a0cf86ce029b1570ee98d8cb2d8ca20d
Josh Anderson <janderson@surgeforward.com>
2021-04-01 00:37:54 -0500

    no message

--- README.md
+++ README.md
@@ -9,5 +9,11 @@ Boilerplate to extend Node applications created by the [Shopify-App-CLI](https:/
 # Installation
 
 1. Create the application within the 'embedded' directory using the 'shopify create' command of the 'shopify cli' tool.
-2. Sign up (or sign into) an Ngrok account (used for public url's to your dev machine). Provision two ngrok url's for use with this application (one for embedded app, one for public app).
-3. Copy '.env.eample' to '.env' in the root directory. Update the APP_URL and EMBEDDED_URL to the Ngrok url's you provisioned above.
+2. Run 'shopify version' to determine the version of the CLI in use.
+3. Copy 'package.json' from /embedded/sea/VERSION_NUMBER to /embedded
+4. Copy 'server.ts' from /embedded/sea/VERSION_NUMBER/server to /embedded/server
+5. Copy 'index.ts'
+
+6. Sign up (or sign into) an Ngrok account (used for public url's to your dev machine). Provision two ngrok url's for use with this application (one for embedded app, one for public app).
+7. Copy '.env.eample' to '.env' in the root directory. Update the APP_URL and EMBEDDED_URL to the Ngrok url's you provisioned above.
+8. Run `docker-compose build` to build your docker containers
--- embedded/pages/index.tsx
+++ embedded/pages/index.tsx
@@ -1,91 +1,9 @@
-import React from 'react';
-import getConfig from 'next/config';
-const { publicRuntimeConfig } = getConfig();
-import isEmail from 'validator/lib/isEmail';
-import _ from 'lodash';
-import { Page, Heading, Card, TextField, FormLayout, Layout, Button } from '@shopify/polaris';
+import { Heading, Page } from '@shopify/polaris';
 
-import ShopifyAdminApiClient from 'lib/api-client/ShopifyAdminApiClient';
-import { responseInterface, ShopResponse } from 'lib/api-client/ShopifyAdminApiResponses';
+const Index = () => (
+  <Page>
+    <Heading>Shopify app with Node and React 🎉</Heading>
+  </Page>
+);
 
-let shopifyAdminApiClient = new ShopifyAdminApiClient();
-
-const IndexPage = () => {
-  let shopSwr: responseInterface<ShopResponse, any> = shopifyAdminApiClient.getShop().toSwr();
-  const defaultEmail = 'sender-email@shop.com';
-  const [email, setEmail] = React.useState(defaultEmail);
-  const [emailError, setEmailError] = React.useState<string | null>(null);
-
-  if (shopSwr.error) return 'An error has occurred.';
-  if (!shopSwr.data) return 'Loading...';
-
-  if (email == defaultEmail) {
-    setEmail(shopSwr.data.customer_email);
-  }
-
-  return (
-    <Page>
-      <Layout>
-        <Layout.AnnotatedSection title="Verify your email" description="Review the email account you'll be using.">
-          <Card sectioned>
-            <FormLayout>
-              <p>
-                By default, we sync order timelines to the <strong>sender email</strong> specified in your{' '}
-                <a href={'https://' + shopSwr.data.domain + '/admin/settings/general'} target="_parent">
-                  general site settings
-                </a>
-              </p>
-              <p>
-                However, if that email account is forwarded to another, you can update the address we use here. The
-                address specified here must recieve any replies a customer makes to the order emails that they recieve
-                for this application to work.
-              </p>
-
-              <TextField
-                type="email"
-                placeholder="Sender Email"
-                value={email}
-                onChange={(email: string) => {
-                  setEmail(email);
-                  if (!isEmail(email)) {
-                    setEmailError('A valid email address is required.');
-                  } else {
-                    setEmailError(null);
-                  }
-                }}
-                label="Sender email"
-                error={emailError}
-              />
-            </FormLayout>
-          </Card>
-        </Layout.AnnotatedSection>
-        <Layout.AnnotatedSection
-          title="Authorize us"
-          description="Authorize our application to use your email account."
-        >
-          <Card sectioned>
-            <p>
-              We do not store your account credentials directly.
-              <br />
-              Rather, you login to your email account, and grant our application access via oauth.
-            </p>
-            <br />
-            <Button
-              url={`https://api.nylas.com/oauth/authorize?login_hint=${encodeURIComponent(email)}&client_id=${
-                publicRuntimeConfig.nylasClientId
-              }&response_type=token&redirect_uri=${encodeURIComponent(
-                publicRuntimeConfig.host + '/nylas-api/callback'
-              )}&scopes=email.send,email.read_only&state=CSRF_TOKEN`}
-              primary
-              disabled={!!emailError}
-            >
-              Grant access to your account
-            </Button>
-          </Card>
-        </Layout.AnnotatedSection>
-      </Layout>{' '}
-    </Page>
-  );
-};
-
-export default IndexPage;
+export default Index;
